c35be0351e363f4bed91e4e44e378845e458b956
Alexoriginal <Superhata23@gmail.com>
2018-01-08 22:46:26 +0200

    3

---
+++ src/Method2.java
@@ -0,0 +1,15 @@
+public class Method2 {
+    public static void main(String[] args) {
+numberFive(9);
+    }
+    public static void numberFive(int a){
+        boolean num=true;
+        if(a>=5){
+            System.out.println(num);
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+
+}
